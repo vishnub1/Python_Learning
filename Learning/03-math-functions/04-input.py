779407df3690695da9b12d614a1edca26f2ea9f6
vishnubiradar <vishnubiradar576@gmail.com>
2025-02-12 08:20:47 +0530

    if else learning

---
+++ Learning/03-math-functions/04-input.py
@@ -0,0 +1,14 @@
+import sys
+
+x = int(input("Enter a number:"))
+y = int(input("Enter a number:"))
+print(x + y)
+
+# eval expression
+result = eval(input("Enter an expression: "))
+print(result)
+
+# # argv
+# a = int(sys.argv[1])
+# b = int(sys.argv[2])
+# print(a + b)
---
+++ Learning/05-Conditional-and-looping/01-if-else.py
@@ -0,0 +1,9 @@
+# Example of if-else usage
+x = int(input("Enter a number: "))
+
+if x > 0:
+    print("The number is positive.")
+elif x < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
