8c7c0d155ff66e71be4591610b48ea1e3912ff66
vishnubiradar <vishnubiradar576@gmail.com>
2025-02-20 00:39:20 +0530

    val

--- Learning/06-Arrays/02-.py
+++ Learning/06-Arrays/02-.py
@@ -2,7 +2,13 @@ from array import *
 
 vals = array('i',[1,2,3,4,5])
 
-newArr = array(vals.typecode,(a for a in vals))
+# printing particular index value
+val = int(input("Enter the value fro search"))
+k = 0
+for e in arr:
+    if e == val:
+        print("The value is present at index", k)
+        break
 
 for e in newArr:
     print(e)
