baa971ba5a37b0f3b43779fc54495b2690e255c7
vishnubiradar <vishnubiradar576@gmail.com>
2025-02-27 08:10:49 +0530

    commit

--- Learning/py_09_OOPs/py_03_constructor.py
+++ Learning/py_09_OOPs/py_03_constructor.py
@@ -1,9 +1,33 @@
-class Com:
-    pass
-
-c1 = Com()
-c2 = Com()
-print(id(c2))
-print(id(c1))
+# class Com:
+#     pass
+#
+# c1 = Com()
+# c2 = Com()
+# print(id(c2))
+# print(id(c1))
 
 # Every time you create an object it is allocated to new space
+
+
+# Compare
+
+class Computer:
+
+    def __int__(self):
+        self.name = "Navin"
+        self.age = 20
+
+    def update(self):
+        self.age = 30
+
+    def compare(self,other):
+        if self.age == other.age:
+            return True
+        else :
+            return False
+
+c1 = Computer()
+c2 = Computer()
+
+if c1.compare(c2):
+    print("They are smme")
